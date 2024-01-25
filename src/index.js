c3449d90722c5b5282a2d19f1825babacb3847b0
m2357aaa <m2357kinu8810@yahoo.co.jp>
2024-01-25 20:00:59 +0900

    menuページ作成

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./common.js"
 import "./css/top.css";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 let kv_title = gsap.timeline({
     scrollTrigger: {
         trigger: "#kv",
@@ -10,4 +14,27 @@ let kv_title = gsap.timeline({
 });
 kv_title
     .from(".kv_title1", { y: 30, opacity: 0, duration: 2 })
-    .from(".kv_title2", { y: 30, opacity: 0, duration: 1 }, "-=1.5");
+    .from(".kv_title2", { y: 30, opacity: 0, duration: 2 }, "-=1.5");
+
+
+let kv_news = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".kv_news",
+        start: "-=600px",
+        scrub: false,
+    },
+});
+kv_news
+    .from(".kv_news", { y: 30, opacity: 0, duration: 2 })
+
+
+let kv_bottom_text = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".kv_bottom_text",
+        start: "-=600px",
+        scrub: false,
+        // markers:true,
+    },
+});
+kv_bottom_text
+    .from(".kv_bottom_text", { y: 30, opacity: 0, duration: 2 })
