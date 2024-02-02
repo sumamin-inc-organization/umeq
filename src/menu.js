55245582fe117ebef8780074a2837744182e9072
Shoma Kumagawa <s.kumagawa@sumamin.com>
2024-02-02 20:26:20 +0900

    menuページのモーダル修正

--- src/menu.js
+++ src/menu.js
@@ -10,6 +10,11 @@ window.showModalWindow = (modalType) => {
     modalType.style.display = "flex";
     fixedHeader.style.position = "relative";
     spHeader.style.position = "relative";
+    document.body.style.overflow = 'hidden';
+
+    addEventListener('click',function(e) {
+        outsideClose(e,modalType);
+    },false);
 }
 
 // モーダルウィンドウを閉じる
@@ -18,4 +23,16 @@ window.closeModal = (modalType) => {
     modalType.style.display = "none";
     fixedHeader.style.position = "fixed";
     spHeader.style.position = "fixed";
+    document.body.style.overflow = 'auto';
+}
+
+// モーダルコンテンツ以外がクリックされた時
+//addEventListener('click', outsideClose);
+function outsideClose(e, modalType) {
+    console.log("click");
+    console.log(modalType);
+    if (e.target == modalType) {
+        modalType.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }
 }
