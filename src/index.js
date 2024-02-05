6b85a2c0ba9c141ec4a7b90fde81ade14bf307d9
m2357aaa <m2357kinu8810@yahoo.co.jp>
2024-02-05 15:49:09 +0900

    ハンバーガーメニュー作成

--- src/index.js
+++ src/index.js
@@ -18,10 +18,14 @@ kv_title
 .from(".kv_title1", { y: -30, opacity: 0, duration: 2 })
 .from(".kv_title2", { y: -30, opacity: 0, duration: 2 }, "-=1.5")
 .from(".slide_show", { y: 30, opacity: 0, duration: 2 }, "-=1.8")
-.from(".kv_news", { y: 30, opacity: 0, duration: 2 }, "-=1.5")
+.from(".kv_news", { y: 30, opacity: 0, duration: 2 }, "-=1.5");
+kv_title
 .from(".kv_deco_kumo_l", { x: 30, opacity: 0, duration: 1 }, "-=1.5")
 .from(".kv_deco_kumo_r", { x: -30, opacity: 0, duration: 1 }, "-=1")
-.from(".kv_deco_ume_l", { scale:1.5, opacity: 0, duration: 1 }, "-=1");
+.from(".kv_deco_ume_l", { scale:0, opacity: 0, duration: 1.8 }, "-=1.5")
+.from(".kv_deco_ume_r", { scale:0, opacity: 0, duration: 1.8 }, "-=1.5")
+.from(".kv_deco_ume_l_small", { scale:0, opacity: 0, duration: 1.8 },"-=1.5")
+.from(".kv_deco_ume_r_small", { scale:0, opacity: 0, duration: 1.8 },"-=1.5");
 
 // kv bottom text
 let kv_bottom_text = gsap.timeline({
