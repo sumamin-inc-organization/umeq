5be28cc89d83167b727337ff054a19365e6e4eec
m2357aaa <m2357kinu8810@yahoo.co.jp>
2024-02-06 14:04:55 +0900

    出現アニメーション追加

--- src/index.js
+++ src/index.js
@@ -37,22 +37,101 @@ let kv_bottom_text = gsap.timeline({
     },
 });
 kv_bottom_text
-.from(".kv_bottom_text", { y: 30, opacity: 0, duration: 2 })
+.from(".kv_bottom_text", { y: 30, opacity: 0, duration: 2 });
 
 // concept
 let concept = gsap.timeline({
     scrollTrigger: {
-        trigger: ".#concept",
-        start: "-=800px",
+        trigger: "#concept",
+        start: "-=600px",
         scrub: false,
-        // markers:true,
     },
 });
 concept
+.from("#concept .top_title", { y: 30, opacity: 0, duration: 1.5 })
+.from("#concept .title_deco_top", { x: 30, opacity: 0, duration: 1 },"-=1")
+.from("#concept .title_deco_bottom", { x: -30, opacity: 0, duration: 1 },"-=1")
+.from(".concept_flex div h3", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".concept_line", { y: 30, opacity: 0, duration: 1.5 },"-=1.5")
+.from(".concept_text", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".concept_btn", { y: 30, opacity: 0, duration: 1.5 },"-=1");
 
-.from(".top_title", { y: 30, opacity: 0, duration: 2 }, "-=1.5")
-.from(".title_deco_top", { x: 30, opacity: 0, duration: 2 }, "-=1.5")
-.from(".title_deco_bottom", { x: -30, opacity: 0, duration: 2 }, "-=1.5")
-.from(".concept_flex h3", { y: 30, opacity: 0, duration: 2 }, "-=1.5")
-.from(".concept_flex img", { y: 30, opacity: 0, duration: 2 }, "-=1.5")
-.from(".concept_flex p", { y: 30, opacity: 0, duration: 2 }, "-=1.5")
+// concept_img
+let concept_img = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".concept_img",
+        start: "-=800px",
+        scrub: false,
+    },
+});
+concept_img
+.from(".concept_img3", { y: 30, opacity: 0, duration: 1.5 })
+.from(".concept_img2", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".concept_img1", { y: 30, opacity: 0, duration: 1.5 },"-=1");
+
+// recommend
+let recommend = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#recommend",
+        start: "-=600px",
+        scrub: false,
+        markers:true,
+    },
+});
+recommend
+.from("#recommend .top_title", { y: 30, opacity: 0, duration: 1.5 })
+.from("#recommend .title_deco_top", { x: -30, opacity: 0, duration: 1 },"-=1")
+.from("#recommend .title_deco_bottom", { x: 30, opacity: 0, duration: 1 },"-=1")
+.from(".recommend_text", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".recommend_menu_kamameshi", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".recommend_menu_yakitori", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".recommend_menu_unagi", { y: 30, opacity: 0, duration: 1.5 },"-=1");
+
+// certification
+let certification = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#certification",
+        start: "-=800px",
+        scrub: false,
+        markers:true,
+    },
+});
+certification
+.from("#certification", { y: 30, opacity: 0, duration: 2.5 })
+.from(".certification_title", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".certification_flex1 img", { y: 30, opacity: 0, duration: 1.5 })
+.from(".certification_flex1 .certification_text", { y: 30, opacity: 0, duration: 1.5 },"-=1")
+.from(".certification_flex2 .certification_text", { y: 30, opacity: 0, duration: 1.5 },"-=0.5")
+.from(".certification_flex2 img", { y: 30, opacity: 0, duration: 1.5 },"-=1");
+
+// news
+let news = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#news",
+        start: "-=700px",
+        scrub: false,
+        markers:true,
+    },
+});
+news
+.from("#news .top_title", { y: 30, opacity: 0, duration: 1.5 })
+.from("#news .title_deco_top", { x: 30, opacity: 0, duration: 1 },"-=1")
+.from("#news .title_deco_bottom", { x: -30, opacity: 0, duration: 1 },"-=1")
+.from(".news_content", { y: 30, opacity: 0, duration: 1.5 },"-=0.5")
+.from(".news_btn", { y: 30, opacity: 0, duration: 1.5 },"-=0.5");
+
+// instagram
+let instagram = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#instagram",
+        start: "-=800px",
+        scrub: false,
+        markers:true,
+    },
+});
+instagram
+.from("#instagram .top_title", { y: 30, opacity: 0, duration: 1.5 })
+.from("#instagram .title_deco_top", { x: -30, opacity: 0, duration: 1 },"-=1")
+.from("#instagram .title_deco_bottom", { x: 30, opacity: 0, duration: 1 },"-=1")
+.from(".insta_content", { y: 30, opacity: 0, duration: 1.5 },"-=0.5")
+.from(".instagram_btn", { y: 30, opacity: 0, duration: 1.5 });
